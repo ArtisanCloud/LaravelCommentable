7541b7191193d82478eef4a3a90cd0187d89c76e
michaelhu <hbj148@gmail.com>
2020-10-18 00:15:42 +0800

    chanage provider bug

---
+++ Providers/CommentableServiceProvider.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace ArtisanCloud\Commentable\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use App\Services\CommentableService\Contracts\CommentableServiceContract;
+
+
+/**
+ * Class CommentableServiceProvider
+ * @package App\Providers
+ */
+class CommentableServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+            // publish config file
+
+            // register artisan command
+            if (!class_exists('CreateCommentableTable')) {
+                $this->publishes([
+                    __DIR__ . '/../database/migrations/create_tag_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_tag_table.php'),
+                    // you can add any number of migrations here
+                ], ['ArtisanCloud', 'Commentable-Migration']);
+            }
+        }
+    }
+}
